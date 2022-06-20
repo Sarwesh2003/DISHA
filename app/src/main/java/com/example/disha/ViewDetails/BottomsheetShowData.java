56ce1471d3e498b72595af1446ae511247146f79
Sarwesh2003 <sarweshkhairnar@gmail.com>
2022-06-20 15:13:28 +0530

    Adding Icon

--- app/src/main/java/com/example/disha/ViewDetails/BottomsheetShowData.java
+++ app/src/main/java/com/example/disha/ViewDetails/BottomsheetShowData.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.disha.R;
 import com.example.disha.AddPlace.data.DAOPlaceData;
@@ -21,6 +22,7 @@ import com.example.disha.AddPlace.data.PlaceData;
 import com.example.disha.AddPlace.data.SliderAdapter;
 import com.example.disha.AddPlace.data.SliderData;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.firebase.storage.FirebaseStorage;
@@ -116,12 +118,12 @@ public class BottomsheetShowData extends BottomSheetDialogFragment {
         listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
             @Override
             public void onSuccess(ListResult listResult) {
+
                 for(StorageReference file:listResult.getItems()){
                     file.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                         @Override
                         public void onSuccess(Uri uri) {
                             sliderDataArrayList.add(new SliderData(uri.toString()));
-                            Log.e("Itemvalue",uri.toString());
                         }
                     }).addOnSuccessListener(new OnSuccessListener<Uri>() {
                         @Override
@@ -133,8 +135,18 @@ public class BottomsheetShowData extends BottomSheetDialogFragment {
                             sliderView.setAutoCycle(true);
                             sliderView.startAutoCycle();
                         }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                        }
                     });
                 }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Something Went Wrong", Toast.LENGTH_SHORT).show();
             }
         });
     }
